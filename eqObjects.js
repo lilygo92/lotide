7b57f97ff10ff064d240c890f83fcc0f160b370b
Lily Go <lilygo7192@gmail.com>
2023-03-08 15:56:55 -0800

    Removed a layer of nesting from conditional checking arrays

--- eqObjects.js
+++ eqObjects.js
@@ -33,10 +33,8 @@ const eqObjects = function(object1, object2) {
     if (object1[key] !== object2[key] && !Array.isArray(object1[key])) {
       return false;
     }
-    if (Array.isArray(object1[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
-        return false;
-      }
+    if (Array.isArray(object1[key]) && !eqArrays(object1[key], object2[key])) {
+      return false;
     }
   }
   return true;
