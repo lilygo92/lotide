dc47a81f4c1d060624c8e2b33e9be74c45559495
Lily Go <lilygo7192@gmail.com>
2023-03-13 15:06:47 -0700

    refactored to use deepEqual instead of eqArrays

--- test/middleTest.js
+++ test/middleTest.js
@@ -4,14 +4,14 @@ const middle = require("../middle");
 
 describe("#middle", () => {
   it("returns [2, 3] for [1, 2, 3, 4]", () => {
-    assert.isTrue(eqArrays(middle([1, 2, 3, 4]), [2, 3]));
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
   });
 
   it("returns [3] for [1, 2, 3, 4, 5]", () => {
-    assert.isTrue(eqArrays(middle([1, 2, 3, 4, 5]), [3]));
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
   });
 
   it("returns [] for [1, 2]", () => {
-    assert.isTrue(eqArrays(middle([1, 2]), []));
+    assert.deepEqual(middle([1, 2]), []);
   });
 })
